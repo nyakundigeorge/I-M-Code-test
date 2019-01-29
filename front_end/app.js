a4f73555c54f0fbf391c25d847d429e19eca3733
nyakundigeorge <gee.nyakundi@gmail.com>
2019-01-29 09:12:04 +0300

    Added skeleton Web interface parts

---
+++ front_end/app.js
@@ -0,0 +1,50 @@
+/**
+ * Author: George Nyakundi
+ * Date: 29/01/2019
+ * Role: Perform queries to https://swapi.co/api/
+ * 
+ * and displays a list of the Star Wars characters’ attributes (at least 3) in a table.
+ *  When a user clicks on an element, they should be able to view all the details for the selected character.
+ *  The user should be allowed to open a character’s details in a new tab.
+ *   The detail view should have a favorites feature where they can pick up to 5 favorites from of the list.
+ * They should also be able to remove a character from their list of favorites.
+ * If a character is marked as a favorite, this should be indicated in the list view with a heart icon.
+
+ */
+
+function loadPage() {
+
+}
+
+/**
+ * 
+ */
+function loadDetails(){
+
+}
+/**
+ * The Details view should have a favorites feature where a user can pick up to 5 favorites.
+ * 
+ */
+function markFavorite() {
+
+}
+
+
+/**
+ * we'll be calling this function to get the favorites from the local storage and append a heart icon next to it.
+ */
+function getFavorites() {
+
+}
+
+/**
+ * This function will be for removing a character from the favorites stored in localstorage
+ */
+function removeFavorites() {
+
+}
+
+function getDetails() {
+
+}
